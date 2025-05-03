46c695566ecd251330beb8066b83727810795303
David Parra <davidc.parrar@gmail.com>
2025-05-03 17:34:53 -0500

    chore: add instrumentation with tracing-subscriber and tracing bunyan-formatter

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, Responder, web};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -17,7 +18,23 @@ pub async fn subscribe(
     let email = &subscription_form.email;
     let id = Uuid::new_v4();
     let subscribed_at = Utc::now();
+    let request_id = Uuid::new_v4();
 
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %email,
+        subscriber_name = %name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    // [...]
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -28,14 +45,23 @@ pub async fn subscribe(
         name,
         subscribed_at
     )
-    // Weuse`get_ref`to getanimmutablereferencetothe`PgConnection`
-    // wrappedby`web::Data`.
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            eprintln!("Failed to execute query: {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
