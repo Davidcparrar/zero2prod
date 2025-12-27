80085ef2dc759fb6a50f0ded42ef90d7c58658b6
David Parra <davidc.parrar@gmail.com>
2025-12-27 15:26:28 -0500

    ci: add updated workflow and database connection for testing

---
+++ src/configuration.rs
@@ -0,0 +1,36 @@
+use config::{Config, ConfigError, File, FileFormat};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub port: u16,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    // Initialise our configuration reader
+    let settings = Config::builder()
+        // Add configuration values from a file named `configuration.yaml`.
+        .add_source(File::new("configuration.yaml", FileFormat::Yaml))
+        .build()?;
+    // Try to convert the configuration values it read into
+    // our Settings type
+    settings.try_deserialize::<Settings>()
+}
