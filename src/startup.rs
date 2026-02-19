f57422a43a6dc413a1bf9dd7ec69a0ce88065f5a
David Parra <davidc.parrar@gmail.com>
2026-02-19 00:02:19 -0500

    refactor: create build fn and simplify main

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
 use actix_web::dev::Server;
-// use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subscribe;
 
-pub fn run(
+fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
@@ -18,7 +19,6 @@ pub fn run(
     let email_client = web::Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
-            // Instead of `Logger::default`
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
@@ -28,4 +28,30 @@ pub fn run(
     .listen(listener)?
     .run();
     Ok(server)
+}
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool =
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    ))?;
+
+    run(listener, connection_pool, email_client)
 }
